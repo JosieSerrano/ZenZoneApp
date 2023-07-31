8e213f95c79ba0fec3996b515c3a17f93c35ced8
Josephine Serrano <josephineserrano07@gmail.com>
2023-07-31 11:50:23 -0400

    Initial Commit

---
+++ ZenZoneApp/ZenZoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenZoneAppApp.swift
+//  ZenZoneApp
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ZenZoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
