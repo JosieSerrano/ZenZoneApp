951c5514270306d6bfd59a3a328c5756ae5c146b
Josephine Serrano <josephineserrano07@gmail.com>
2023-08-01 13:21:46 -0400

    TabView

---
+++ ZenZoneApp/CalenderView.swift
@@ -0,0 +1,20 @@
+//
+//  CalenderView.swift
+//  ZenZoneApp
+//
+//  Created by Scholar on 8/1/23.
+//
+
+import SwiftUI
+
+struct CalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalenderView()
+    }
+}
--- ZenZoneApp/homeView.swift
+++ ZenZoneApp/homeView.swift
@@ -12,7 +12,7 @@ struct homeView: View {
     //Light Peach
     
     let lightPeach = UIColor(red: 0.996078431372549, green: 0.8705882352941177, blue: 0.807843137254902, alpha: 1)
-
+    
     //Tea Green
     
     let teaGreen = UIColor(red: 0.6901960784313725, green: 0.8, blue: 0.7529411764705882, alpha: 1.0)
@@ -25,54 +25,66 @@ struct homeView: View {
     
     let rosyCheeks = UIColor(red: 0.996078431372549, green: 0.8549019607843137, blue: 0.8392156862745098, alpha: 1.0)
     
-
+    @State var tabSelection = 1
     var body: some View {
+    
        
-        NavigationStack{
-        
-            ZStack{
-                Color(teaGreen).ignoresSafeArea()
+               
                 
-                Rectangle()
-                    .frame(width: 180, height: 250)
-                    .cornerRadius(50)
-                    .position(x:150, y:140)
-                    .foregroundColor(Color.white)
+            
+            NavigationStack{
+                
+                
+                               
+                
+                    ZStack{
+                        
+                        Color(teaGreen).ignoresSafeArea()
+                        
+                        
+                        Rectangle()
+                            .frame(width: 180, height: 250)
+                            .cornerRadius(50)
+                            .position(x:150, y:140)
+                            .foregroundColor(Color.white)
+                        
+                        
+                        Rectangle()
+                            .frame(width: 180, height: 250)
+                            .cornerRadius(50)
+                            .position(x:350, y:140)
+                            .foregroundColor(Color.white)
+                        
+                        
+                        Rectangle()
+                            .frame(width: 180, height: 250)
+                            .cornerRadius(50)
+                            .position(x:350, y:575)
+                            .foregroundColor(Color.white)
+                        
+                        
+                        Rectangle()
+                            .frame(width: 180, height: 250)
+                            .cornerRadius(50)
+                            .position(x:150, y:575)
+                            .foregroundColor(Color.white)
+                        
+                        Image("Koala")
+                        
+                    }
+               
+                    }
                     
-                
-                Rectangle()
-                    .frame(width: 180, height: 250)
-                    .cornerRadius(50)
-                    .position(x:350, y:140)
-                    .foregroundColor(Color.white)
+                }
                     
-                
-                Rectangle()
-                    .frame(width: 180, height: 250)
-                    .cornerRadius(50)
-                    .position(x:350, y:575)
-                    .foregroundColor(Color.white)
-                
-                
-                Rectangle()
-                    .frame(width: 180, height: 250)
-                    .cornerRadius(50)
-                    .position(x:150, y:575)
-                    .foregroundColor(Color.white)
-                 
-                Image("Koala")
-                  
-             
-                
-            }
-           
-        }
-    }
+                }
+         
+    
+    
 
-}
 
 struct homeView_Previews: PreviewProvider {
     static var previews: some View {
-        homeView()
+        SwiftUIView()
     }
 }
